f3f4f858bb22428218ff6aed811624a2dd1f24a0
shahdmadhoun <shahdem100@gmail.com>
2022-03-06 10:34:50 -0800

    edit student

--- src/app/get-student/get-student.component.ts
+++ src/app/get-student/get-student.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
 import { Student } from '../student';
 import { Router } from '@angular/router';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
@@ -12,24 +13,61 @@ import { Router } from '@angular/router';
 export class GetStudentComponent implements OnInit {
 
   students: Student[] = [];
-  constructor(private data: DataService) { }
+  closeResult: string = '';
+  name:string = ''
+  email:string = ''
+  age:number = 0
+  gpa:number = 90
+  constructor(private data: DataService, private modalService: NgbModal) { }
 
   ngOnInit(): void {
     this.students = this.data.getStudents();
     console.log(this.students)
   }
+      
+  open(content: any, id:number) {
+    for (let index = 0; index < this.students.length; index++) {
+      if(id == this.students[index].id) {
+        this.name = this.students[index].name;
+        this.email = this.students[index].email;
+        this.age = this.students[index].age;
+        this.gpa = this.students[index].gpa;
+        console.log(this.students[index])
+      }
+    }
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+      console.log(result)
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
 
   public editStudent(id:number){
-    for (let index = 0; index < this.data.getStudents().length; index++) {
-      if(this.students[index].id == id) {
-        this.students[index].name = "new name";
-        this.students[index].gpa = 100;
-        this.students[index].age = 11;
-        this.students[index].email = "new@gmail.com";
+    for (let index = 0; index < this.students.length; index++) {
+      if(id == this.students[index].id) {
+        this.students[index].name = this.name;
+        this.students[index].email = this.email;
+        this.students[index].age = this.age;
+        this.students[index].gpa = this.gpa;
+        console.log(this.students[index])
         return this.data.editStudent(this.students[index]);
       }
     }
+
   }
+
+  
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+
 
   public deleteStudent(id:number){
     for (let index = 0; index < this.data.getStudents().length; index++) {
